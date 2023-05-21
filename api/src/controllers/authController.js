b33b2dcc0ba2f3be6155324bbfbcd8f26c130411
Angjelo <33224333+angjelonuho@users.noreply.github.com>
2023-05-21 20:56:55 +0200

    Update authController.js

--- api/src/controllers/authController.js
+++ api/src/controllers/authController.js
@@ -15,7 +15,7 @@ exports.login = (req, res) => {
     const { username, password } = req.body;
     const user = users.find(u => u.username === username && u.password === password);
     if (!user) {
-        return res.status(401).json({ error: 'Invalid credentials' });
+        return res.status(401).json({ error: 'invalid_credentials' });
     }
 
     const accessToken = signAccessToken(user)
